ed16060d939a05e15b27ca9f731e64b83abe41fe
ceunnseo <eunseo4068@gmail.com>
2024-04-02 01:13:19 +0900

    수정, 삭제버튼

--- src/component/ContactId.js
+++ src/component/ContactId.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import {Row, Col} from "react-bootstrap"
+import {Row, Col, Button} from "react-bootstrap"
 
 const ContactId = ({item}) => {
   return (
     <Row className = "contactId">
-        <Col lg = {2} md = {3}>
+        <Col lg = {2}>
             <img
                 width = {50}
                 src = "https://cdn-icons-png.flaticon.com/512/8847/8847419.png"/>
         </Col>
-        <Col lg = {10} md = {9}>
+        <Col lg = {4}>
             <div>{item.name}</div>
             <div>{item.phoneNumber}</div>
+        </Col>
+        <Col className = "contactId-edit-delete" lg = {6}>
+          <Button>수정</Button>
+          <Button>삭제</Button>
         </Col>
     </Row>
   )
